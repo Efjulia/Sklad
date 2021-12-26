c52ab207a8ba6e015a9c24cbb095d8126fcb23f4
Efjulia <efjulia@mail.ru>
2021-12-26 21:12:43 +0300

    печать штрих кода по размерам

--- Sklad/genbarcode.Designer.cs
+++ Sklad/genbarcode.Designer.cs
@@ -35,14 +35,22 @@ namespace Sklad
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(218, 345);
+            this.button1.Location = new System.Drawing.Point(248, 71);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 36);
+            this.button1.Size = new System.Drawing.Size(186, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "&Печать";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,9 +58,9 @@ namespace Sklad
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(345, 345);
+            this.button2.Location = new System.Drawing.Point(248, 125);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(123, 35);
+            this.button2.Size = new System.Drawing.Size(186, 40);
             this.button2.TabIndex = 1;
             this.button2.Text = "&Генерировать";
             this.button2.UseVisualStyleBackColor = true;
@@ -61,37 +69,118 @@ namespace Sklad
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(33, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(15, 231);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(435, 253);
+            this.pictureBox1.Size = new System.Drawing.Size(200, 100);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 302);
+            this.label1.Location = new System.Drawing.Point(23, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.Size = new System.Drawing.Size(219, 20);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Text = "Текст для штрих-кодирования";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(110, 302);
+            this.textBox1.Location = new System.Drawing.Point(248, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(358, 27);
+            this.textBox1.Size = new System.Drawing.Size(186, 27);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // printDocument1
             // 
             this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ширина";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Высота";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(15, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 163);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Фиксированные размеры";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "X";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "60",
+            "80",
+            "100"});
+            this.comboBox2.Location = new System.Drawing.Point(137, 88);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(70, 28);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.Text = "100";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "160",
+            "180",
+            "200"});
+            this.comboBox1.Location = new System.Drawing.Point(21, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(70, 28);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "200";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(249, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 40);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "&Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // genbarcode
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(483, 399);
+            this.ClientSize = new System.Drawing.Size(459, 359);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
@@ -101,6 +190,8 @@ namespace Sklad
             this.Text = "Штрих-код";
             this.Load += new System.EventHandler(this.genbarcode_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,5 +205,12 @@ namespace Sklad
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Sklad/genbarcode.cs
+++ Sklad/genbarcode.cs
@@ -18,17 +18,21 @@ namespace Sklad
 
         private void button2_Click(object sender, EventArgs e)
         {
+            //button1.Enabled = false;
             if (textBox1.Text.Trim() == "")
             {
                 MessageBox.Show("Ошибка ввода текста", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
             }
-            {
+            {button1.Enabled = true;
                 Barcode barcode = new Barcode();
                 Color foreColor = Color.Black;
                 Color BackColor = Color.Transparent;
                 Image img = barcode.Encode(TYPE.CODE128, textBox1.Text.Trim(), foreColor, BackColor, (int)(pictureBox1.Width * 0.8), (int)(pictureBox1.Height * 0.8));
                 pictureBox1.Image = img;
+                
+              //  Graphics.FromImage(bmp).Clear(Color.Black);
+
             }
 
         }
@@ -42,20 +46,42 @@ namespace Sklad
         
 
         private void genbarcode_Load(object sender, EventArgs e)
-        {textBox1.Focus();
+        {
+            pictureBox1.Width = Convert.ToInt32(comboBox1.Text);
+            pictureBox1.Height = Convert.ToInt32(comboBox2.Text);
+
+            textBox1.Focus();
             this.ActiveControl = textBox1;
             textBox1.MaxLength = 40;//Максимальная длина шифруемого в штрих-код текста
-            
+            button1.Enabled = false;
 
         }
 
         private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
-            e.Graphics.DrawImage(pictureBox1.Image, 0, 0);
+            e.Graphics.DrawString("Название детали", new Font("Times New Roman", 14), Brushes.Black, 10, 10);
+            e.Graphics.DrawImage(pictureBox1.Image, 10, 30);
            /* using (Graphics dg = e.Graphics)
             { 
             
             }*/
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Width = Convert.ToInt32(comboBox1.Text);
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Height = Convert.ToInt32(comboBox2.Text);
         }
     }
 }
